817978f675b80ccbaccd44d42470d05c928563d8
OhJungJae <104138263+wjdwo1112@users.noreply.github.com>
2023-10-16 23:31:47 +0900

    feat:10일차 문제풀이

---
+++ src/baekjoon/bruteforce/Main_백준_2231번_분해합.java
@@ -0,0 +1,36 @@
+package baekjoon.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main_백준_2231번_분해합 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int result = 0;
+
+
+        for(int i = 0; i < N; i++) {
+            int number = i;
+            int sum = 0;	// 각 자릿수 합 변수
+
+            while(number != 0) {
+                sum += number % 10;	// 각 자릿수 더하기
+                number /= 10;
+            }
+
+            // i 값과 각 자릿수 누적합이 같을 경우 (생성자를 찾았을 경우)
+            if(sum + i == N) {
+                result = i;
+                break;
+            }
+
+        }
+
+        System.out.println(result);
+    }
+}
