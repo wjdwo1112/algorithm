df2a18ff0afdc723d9e54095d7484eb1ca54e258
Jung Jae Oh <wjdwo1112@naver.com>
2024-01-04 19:43:25 +0900

    feat:25일차 문제풀이

---
+++ src/baekjoon/implementation/Main_백준_2441번_별찍기4.java
@@ -0,0 +1,19 @@
+package baekjoon.implementation;
+
+import java.util.Scanner;
+
+public class Main_백준_2441번_별찍기4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for(int j=1; i>j; j++){
+                System.out.print(" ");
+            }
+            for (int k = n; k >= i; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
