b10d66e820fb25019e543e2c9d157786163ce12f
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2024-02-09 11:23:26 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -7,37 +7,36 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int[] arr = new int[9];
-
         int sum = 0;
-        int a = 0;
-        int b = 0;
         for(int i=0; i<9; i++){
             arr[i] = Integer.parseInt(bf.readLine());
-            sum += arr[i];
+            sum +=arr[i];
         }
-        for(int i=0; i<8; i++) {
-            for (int j = i + 1; j < 9; j++) {
+        Arrays.sort(arr);
 
-                if (sum - arr[i] - arr[j] == 100) {
-                    a = i;
-                    b = j;
-                    break;
+        int start = 0;
+        int end = 8;
 
-                }
+
+        //투포인터로 품
+        while(start<end){
+            if(sum - arr[start] - arr[end] == 100){
+                break;
+
+            }else if(sum - arr[start] - arr[end] > 100){
+                start++;
+
+            }else if(sum - arr[start] - arr[end] < 100) {
+                end--;
             }
-            Arrays.sort(arr);
-        }
 
+        }
         for(int i=0; i<9; i++){
-            if(i==a || i==b) {
+            if(i == start || i == end){
                 continue;
             }
             System.out.println(arr[i]);
-
         }
-
 
     }
 }
-
-
