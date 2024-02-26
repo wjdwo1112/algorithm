fb518e313b9b7f20f1f75abd8c8f52253f8e193e
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2024-02-26 23:51:38 +0900

    [Bronze IV] Title: 특별한 날, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(bf.readLine());
+        int b = Integer.parseInt(bf.readLine());
+
+        if(a<2){
+            System.out.println("Before");
+
+        }else if(a==2 && b<18){
+            System.out.println("Before");
+        }else if(a==2 && b==18){
+            System.out.println("Special");
+        } else {
+            System.out.println("After");
+        }
+    }
+}
