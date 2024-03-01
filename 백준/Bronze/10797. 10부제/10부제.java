6ceb7bae9c6a94b586195b603a0377b5de47c346
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2024-03-01 15:41:19 +0900

    [Bronze IV] Title: 10부제, Time: 220 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int count=0;
+        for(int i=0;i<5;i++){
+            int a=s.nextInt();
+            if(n==a) count++;
+        }
+        System.out.print(count);
+    }
+}
