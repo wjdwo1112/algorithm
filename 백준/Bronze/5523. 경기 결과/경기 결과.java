89ac5f208129ad6684a3018e57524a3e1cc7231a
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2024-03-24 17:08:22 +0900

    [Bronze III] Title: 경기 결과, Time: 592 ms, Memory: 206064 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5523. 경기 결과/경기 결과.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(bf.readLine());
+		
+		int Awin = 0;
+		int Bwin = 0;
+		
+		for(int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			//값이 더 큰 쪽에 승리 점수를 추가
+			if(A > B) {
+				Awin++;
+			}else if(B > A) {
+				Bwin++;
+			}
+		}
+		System.out.println(Awin + " " + Bwin);
+	}
+
+}
