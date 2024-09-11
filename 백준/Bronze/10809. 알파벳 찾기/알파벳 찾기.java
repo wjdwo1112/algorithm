dfc9043c3454e4caa3106cfd40362a6a61bdb057
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2024-09-11 22:09:04 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 116 ms, Memory: 15876 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String S = bf.readLine();
+
+        int[] alphabet = new int[26];
+        for(int i=0; i<26; i++){
+            alphabet[i] = -1;
+        }
+
+        for(int i=0; i<S.length(); i++){
+            char c = S.charAt(i);
+            if(alphabet[c-97] != -1){
+                continue;
+            }
+            alphabet[c-97] = i;
+        }
+
+        for(int i=0; i<26; i++){
+            System.out.print(alphabet[i] + " ");
+        }
+    }
+}
