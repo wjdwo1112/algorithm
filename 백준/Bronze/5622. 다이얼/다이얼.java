af985f598f2764316edf20f98162fcb2d7ee8e1b
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2024-09-12 17:32:06 +0900

    [Bronze II] Title: 다이얼, Time: 96 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String s = bf.readLine();
+        int cnt = 0;
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            switch (c){
+                case 'A': case 'B': case 'C':
+                   cnt += 3;
+                   break;
+                case 'D': case 'E': case 'F':
+                    cnt += 4;
+                    break;
+                case 'G': case 'H' : case 'I':
+                    cnt += 5;
+                    break;
+                case 'J': case 'K': case'L':
+                    cnt += 6;
+                    break;
+                case 'M': case 'N': case 'O':
+                    cnt += 7;
+                    break;
+                case 'P': case 'Q': case 'R': case'S':
+                    cnt += 8;
+                    break;
+                case 'T': case 'U': case 'V':
+                    cnt += 9;
+                    break;
+                case 'W': case 'X': case 'Y': case 'Z':
+                    cnt += 10;
+                    break;
+
+            }
+
+
+        }
+        System.out.println(cnt);
+
+
+    }
+}
