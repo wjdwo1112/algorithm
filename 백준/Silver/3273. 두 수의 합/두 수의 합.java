1dbc7581d4b4779a19d25d9c3ec2f96037d8a0c8
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2024-10-11 14:16:07 +0900

    [Silver III] Title: 두 수의 합, Time: 344 ms, Memory: 26740 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,37 +8,30 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
         int n = Integer.parseInt(bf.readLine());
-        int[] arr = new int [n];
-        st = new StringTokenizer(bf.readLine(), " ");
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
         for(int i=0; i<n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
-
         }
-        int x = Integer.parseInt(bf.readLine());
 
+        int x = Integer.parseInt(bf.readLine());
         Arrays.sort(arr);
 
+        int cnt = 0;
         int start = 0;
         int end = n-1;
-        int cnt = 0;
-
-        while(start<end){
-            int sum = arr[start] + arr[end];
-            if(sum > x){
-                end = end-1;
-
-            }else if(sum < x){
-                start = start+1;
-
-            }if(sum == x){
-                cnt++;
+        while(start < end){
+            if(arr[start]+arr[end] > x){
                 end--;
+            }else if(arr[start]+arr[end] < x){
                 start++;
+            }else{
+                cnt++;
+                start++;
+                end--;
             }
-
         }
         System.out.println(cnt);
     }
