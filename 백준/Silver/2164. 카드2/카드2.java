a9b92c585c66c4671fb5260ff879776ef0424791
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-02-07 00:31:12 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 53348 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -8,16 +8,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i=1; i<=n; i++){
-            queue.add(i);
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int i=1; i<=n; i++){ //카드를 큐에 저장
+            q.offer(i);
         }
-        while(queue.size() > 1){
-            queue.poll();
-            int temp = queue.poll();
-            queue.offer(temp);
+        while(q.size() > 1){ // 카드가 1장 남을때까지
+            q.poll(); // 맨 위 카드 버리기
+            int temp = q.poll(); // 맨 위 카드를 버리고 저장
+            q.offer(temp); // 맨 위 카드를 가장 아래 카드 밑으로 이동
+
         }
-        System.out.println(queue.poll());
+        System.out.println(q.poll()); // 마지막으로 남은 카드 출력
+
+
 
     }
 }
