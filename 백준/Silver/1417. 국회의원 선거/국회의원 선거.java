c1ba27039d399abbe9995a5013514684f8a6a213
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-03-03 23:21:48 +0900

    [Silver V] Title: 국회의원 선거, Time: 112 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -1,39 +1,37 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-// The main method must be in a class named "Main".
-class Main {
-    public static void main(String[] args) throws IOException{
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
-        int dasom = Integer.parseInt(bf.readLine());
         int[] arr = new int[n-1];
+        int dasom = Integer.parseInt(bf.readLine());
         for(int i=0; i<n-1; i++){
             arr[i] = Integer.parseInt(bf.readLine());
+
         }
-       int cnt = 0;
+        int count = 0;
+
+
         if(n==1){
             System.out.println(0);
             return;
         }
+
         while(true){
             Arrays.sort(arr);
-            boolean flag = true;
-            int last = arr.length-1;
-            if(dasom<=arr[last]){
-                dasom++;
-                arr[last]--;
-                cnt++;
-                flag = false;
-            }
-            if(flag){
+            if(dasom > arr[n-2]){
                 break;
             }
-            
+            dasom++;
+            arr[n-2]--;
+            count++;
         }
-        System.out.println(cnt);
-        
-        
+        System.out.println(count);
     }
 }
