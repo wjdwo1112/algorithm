bc14c258dbf7e746b75a9511ed871a4575a8cd64
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-04-05 23:56:13 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 172 ms, Memory: 17844 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner s=new Scanner(System.in);
+        int d1=s.nextInt();
+        int d2=s.nextInt();
+        double pi=3.141592;
+        System.out.format("%f",2*d1+2*pi*d2);
+    }
+}
