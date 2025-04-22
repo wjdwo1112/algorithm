4b74d40067e0b192ccb769582bb8eafde193b224
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-04-22 19:53:36 +0900

    [D1] Title: 평균값 구하기, Time: 85 ms, Memory: 24,960 KB -BaekjoonHub

--- SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
@@ -1,29 +1,28 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-// The main method must be in a class named "Main".
-class Solution {
-    public static void main(String[] args) throws IOException{
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Solution{
+    public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st ;
+        StringTokenizer st;
+
         int T = Integer.parseInt(bf.readLine());
-        int[] arr = new int[10];
-        
-        double avg = 0;
-        
-        for(int i=0; i<T; i++){
-            double sum = 0;
+        for(int t=1; t<=T; t++){
+            int[] arr = new int[10];
+            int sum = 0;
             st = new StringTokenizer(bf.readLine());
-            for(int j=0; j<10; j++){
+            for(int i=0; i<10; i++){
                 arr[i] = Integer.parseInt(st.nextToken());
                 sum += arr[i];
             }
-            avg = sum/10;
-            System.out.println("#"+ (i+1) + " " + Math.round(avg));
-            
-            
+            double avg = (double) sum / 10;
+            System.out.println("#" + t + " " + String.format("%.0f",avg));
+
         }
-        
     }
 }
