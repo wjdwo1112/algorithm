5a088dbc1a85b14817a3218f56bd881da7829609
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-04-26 16:37:56 +0900

    [D2] Title: 간단한 369게임, Time: 85 ms, Memory: 25,728 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -1,28 +1,28 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
-        int i = 1;
 
-        while(i <= n){
-            
-            String I = String.valueOf(i);
-            if(I.contains("3") || I.contains("6") || I.contains("9")){
-                for(int j=0; j<I.length(); j++){
-                    if(I.charAt(j) == '3' || I.charAt(j) == '6' ||I.charAt(j) == '9'){
-                        System.out.print('-');
+        for(int i=1; i<=n; i++){
+            String a = String.valueOf(i);
+            if(a.contains("3") || a.contains("6") || a.contains("9")) {
+                for (int j = 0; j < a.length(); j++) {
+                    if (a.charAt(j) == '3' || a.charAt(j) == '6' || a.charAt(j) == '9') {
+                        System.out.print("-");
                     }
                 }
             }else{
-                System.out.print(I);
+                System.out.print(a);
             }
             System.out.print(" ");
-            i++;
+
+
         }
     }
 }
