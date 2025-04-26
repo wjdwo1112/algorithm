3a33b07498e8297566417202e07e6c8b21631e81
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-04-26 16:09:29 +0900

    [D2] Title: 수도 요금 경쟁 D2 Attack, Time: 74 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.java
+++ SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,25 +8,28 @@ import java.util.StringTokenizer;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st ;
         int T = Integer.parseInt(bf.readLine());
-        for(int t=1; t<=T; t++){
-            StringTokenizer st = new StringTokenizer(bf.readLine());
-            int p = Integer.parseInt(st.nextToken());
-            int q = Integer.parseInt(st.nextToken());
-            int r = Integer.parseInt(st.nextToken());
-            int s = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
 
-            int a = p * w;
-            int b = 0;
-            if(w <= r){
-                b = q;
-            }else{
-                b = q + s * (w-r);
+        for(int t=1; t<=T; t++){
+            st = new StringTokenizer(bf.readLine());
+            int P = Integer.parseInt(st.nextToken());
+            int Q = Integer.parseInt(st.nextToken());
+            int R = Integer.parseInt(st.nextToken());
+            int S = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+
+
+            int A = P * W;
+
+            int B = Q;
+            if(W > R){
+                B = B + (W-R)*S;
             }
-            int result = Math.min(a,b);
-            System.out.println("#" + t + " " + result);
+
+            int result = Math.min(A,B);
+
+            System.out.println("#" + t+ " " + result);
         }
     }
-
 }
