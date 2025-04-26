96f1d0da86829f0282e33f03a70d95a124f103fa
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-04-26 19:11:14 +0900

    [D2] Title: 중간 평균값 구하기, Time: 81 ms, Memory: 25,728 KB -BaekjoonHub

--- SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
+++ SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,22 +9,23 @@ import java.util.StringTokenizer;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         int T = Integer.parseInt(bf.readLine());
+
         for(int t=1; t<=T; t++){
             int[] arr = new int[10];
-            StringTokenizer st = new StringTokenizer(bf.readLine());
-            for(int i=0; i<10; i++) {
+            st = new StringTokenizer(bf.readLine());
+            for(int i=0; i<10; i++){
                 arr[i] = Integer.parseInt(st.nextToken());
             }
             Arrays.sort(arr);
-            double sum = 0;
+            double avg = 0;
+            int sum = 0;
             for(int i=1; i<9; i++){
                 sum += arr[i];
             }
-            double avg = Math.round(sum / 8); //Math.round는 소수점 첫째자리에서 반올림하는 것
-            int result = (int)avg;
-
-            System.out.println("#" + t + " " + result);
+            avg = (double)sum / 8;
+            System.out.println("#" + t + " " + String.format("%.0f",avg));
         }
     }
 }
