15d70dcc35eacf9fec861b14c79e4923c636524b
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-04-27 17:03:34 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 102 ms, Memory: 26,880 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,46 +1,32 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         for(int t=1; t<=10; t++){
-            int freq = Integer.parseInt(bf.readLine());
-            int[] arr = new int[100];
-            StringTokenizer st = new StringTokenizer(bf.readLine());
-            for(int i=0; i<100; i++){
+            int n = Integer.parseInt(bf.readLine());
+            int[] arr = new int[101];
+            st = new StringTokenizer(bf.readLine());
+            for(int i=1; i<=100; i++){
                 arr[i] = Integer.parseInt(st.nextToken());
             }
-
-            int result = 0;
-            for(int i=0; i<freq; i++){
-                int maxIndex = 0;
-                int minIndex = 0;
-                for(int j=0; j<100; j++){
-                    if(arr[j] > arr[maxIndex]){
-                        maxIndex = j;
-                    }
-                    if(arr[j] < arr[minIndex]){
-                        minIndex = j;
-                    }
-                }
-                if(arr[maxIndex] - arr[minIndex] <= 1){
-                    break;
-                }
-                arr[maxIndex]--;
-                arr[minIndex]++;
+            Arrays.sort(arr);
+            int i = 1;
+            while(i<=n){
+                arr[100]--;
+                arr[1]++;
+                Arrays.sort(arr);
+                i++;
             }
-            int max = arr[0];
-            int min = arr[0];
-            for(int i=0; i<100; i++){
-                max = Math.max(arr[i],max);
-                min = Math.min(arr[i],min);
-            }
-            result = max - min;
+            int result = arr[100] - arr[1];
             System.out.println("#" + t + " " + result);
-
         }
     }
 }
