27b786e2191da7c5961e8b2e409964b271e2ae75
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-04-28 17:37:52 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 99 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -1,33 +1,37 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution{
+public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
         for(int t=1; t<=10; t++){
             int n = Integer.parseInt(bf.readLine());
-
             int[] arr = new int[n];
-            StringTokenizer st = new StringTokenizer(bf.readLine());
-
+            st = new StringTokenizer(bf.readLine());
             for(int i=0; i<n; i++){
                 arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            int cnt = 0;
+            int sum = 0;
+            for(int i=2; i<n-2; i++){
+                int max1 =0;
+                int max2 = 0;
+                int max = 0;
+                if(arr[i] > arr[i-2] && arr[i] > arr[i-1] && arr[i] > arr[i+1] && arr[i] > arr[i+2]){
+                    max1 = Math.max(arr[i-2],arr[i-1]);
+                    max2 = Math.max(arr[i+1],arr[i+2]);
+                    max = Math.max(max1,max2);
 
-            for(int i=2; i<n-2; i++) {
-                int leftMax = Math.max(arr[i-1],arr[i-2]); //arr[i] 기준 왼쪽 최대값(i-2까지)
-                int rightMax = Math.max(arr[i+1],arr[i+2]); // arr[i] 기준 오른쪽 최대값(i+2까지)
-                int max = Math.max(leftMax,rightMax);
-                if(arr[i] > max){ // arr[i] 에서 뺀 값이 2이상이면
-                    cnt = cnt + (arr[i] - max); // 왼쪽 최대값과 오른쪽 최대값중 더 큰걸 뺸걸 더해준다.
+                    sum += (arr[i] - max);
                 }
-
             }
-            System.out.println("#" + t + " " + cnt);
+             System.out.println("#" + t + " " + sum);
         }
     }
 }
