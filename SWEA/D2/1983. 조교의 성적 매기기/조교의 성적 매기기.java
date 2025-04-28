4716b8f7dfcdb4e5b2f53aae5c4640d9856c1302
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-04-28 19:54:56 +0900

    [D2] Title: 조교의 성적 매기기, Time: 93 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.java
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,45 +11,38 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int t = Integer.parseInt(bf.readLine());
+        int T = Integer.parseInt(bf.readLine());
 
-        String[] score = new String[]{"A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"};
-
-        for(int m=0; m<t; m++){
-             st = new StringTokenizer(bf.readLine());
+        for(int t=1; t<=T; t++){
+            st = new StringTokenizer(bf.readLine());
             int n = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken()) - 1; // 배열은 0부터 시작이라 입력값 들어갈때 1빼준다
-            Double [] arr = new Double[n];
-            double targetscore = 0;
-
+            int k = Integer.parseInt(st.nextToken());
+            Double[] arr = new Double[n];
+            String[] grade = {"A+","A0","A-","B+","B0","B-","C+","C0","C-","D0"};
             for(int i=0; i<n; i++){
                 st = new StringTokenizer(bf.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
                 int c = Integer.parseInt(st.nextToken());
-                double total = a * 0.35 + b * 0.45 + c * 0.2;
-                arr[i] = total;
-                if(i == k){
-                    targetscore = total; // k번쨰 학생의 점수 저장
-                }
-            }
-            Arrays.sort(arr, Collections.reverseOrder()); //내림차순 정렬
+                arr[i] = a*0.35 + b*0.45 + c*0.20;
 
-            //k번째 학생 순위 계산
+            }
+
+            double targetScore = arr[k-1];
+
+            Arrays.sort(arr,Collections.reverseOrder());
+
             int rank = 0;
             for(int i=0; i<n; i++){
-                if(arr[i] == targetscore){
+                if(arr[i] == targetScore){
                     rank = i;
                     break;
                 }
             }
-            //등급 비율에 따를 인덱스 계산
             int gradeIndex = (rank * 10) / n;
-            System.out.printf("#%d %s%n", m+1, score[gradeIndex]);
-
+            System.out.println("#" + t + " " + grade[gradeIndex]);
 
 
         }
-
     }
 }
