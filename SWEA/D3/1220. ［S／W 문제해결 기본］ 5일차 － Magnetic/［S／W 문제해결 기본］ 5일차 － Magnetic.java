c61805abed6abdcadb76822f2187ea38ac118657
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-04-30 22:37:27 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 129 ms, Memory: 36,572 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,32 +8,39 @@ import java.util.StringTokenizer;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        for(int t=1; t<=10; t++){
-            int[][] arr = new int[100][100];
+        StringTokenizer st;
 
+        for(int t=1; t<=10 ;t++){
             int n = Integer.parseInt(bf.readLine());
+            int[][] arr = new int[n+1][n+1];
 
-            for(int i=0; i<100; i++){
-                StringTokenizer st = new StringTokenizer(bf.readLine());
-                for(int j=0; j<100; j++){
+            for(int i=1; i<=n; i++){
+                st = new StringTokenizer(bf.readLine());
+                for(int j=1; j<=n; j++){
                     arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
             int cnt = 0;
 
-            for(int j=0; j<100; j++){
+            for(int i=1; i<=n; i++){
+
                 boolean check = false;
-                for(int i=0; i<100; i++){
-                    if(arr[i][j] == 1){
+
+                for(int j=1; j<=n; j++){
+
+                    if(arr[j][i] == 1){
                         check = true;
-                    }
-                    if(check == true && arr[i][j] == 2){
-                        cnt ++;
+                    }else if(check == true && arr[j][i] == 2){
+                        cnt++;
                         check = false;
                     }
+
                 }
+
+
             }
+
             System.out.println("#" + t + " " + cnt);
         }
     }
