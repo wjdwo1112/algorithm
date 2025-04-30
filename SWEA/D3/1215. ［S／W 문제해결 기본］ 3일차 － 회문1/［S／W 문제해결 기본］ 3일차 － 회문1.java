9f2367f71499fe5a8541d7b933c7d3972729a203
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-05-01 00:15:41 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 86 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,54 +8,48 @@ import java.util.StringTokenizer;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         for(int t=1; t<=10; t++){
-            int a = Integer.parseInt(bf.readLine());
+            int n = Integer.parseInt(bf.readLine());
             char[][] arr = new char[8][8];
-
             for(int i=0; i<8; i++){
-                StringTokenizer st = new StringTokenizer(bf.readLine());
+                st = new StringTokenizer(bf.readLine());
                 String line = st.nextToken();
                 for(int j=0; j<8; j++){
                     arr[i][j] = line.charAt(j);
                 }
             }
-
-            int cnt = 0;
-
-            // 행 회문 검사
+            int result = 0;
+            //행 회문 검사
             for(int i=0; i<8; i++){
-                for(int j=0; j<=8-a; j++){
-                    boolean isPalindrome = true;
-                    for(int k=0; k<a/2; k++) {
-                        if(arr[i][j+k] != arr[i][j+ (a-1-k)]){
-                            isPalindrome = false;
-                            break;
-                        }
+                for(int j=0; j<=8-n; j++){
+                    int cnt = 0;
+                    for(int k=0; k<n/2; k++){
+                     if(arr[i][j+k] == arr[i][j+n-1-k]){
+                         cnt++;
+                     }
                     }
-                    if(isPalindrome){
-                        cnt++;
+                    if(cnt == n/2){
+                        result++;
                     }
                 }
             }
 
             //열 회문 검사
             for(int j=0; j<8; j++){
-                for(int i=0; i<=8-a; i++){
-                    boolean isPalindrome = true;
-                    for(int k=0; k<a/2; k++) {
-                        if(arr[i+k][j] != arr[i+(a-1-k)][j]){
-                            isPalindrome = false;
-                            break;
+                for(int i=0; i<=8-n; i++){
+                    int cnt = 0;
+                    for(int k=0; k<n/2; k++){
+                        if(arr[i+k][j] == arr[i+n-1-k][j]){
+                            cnt++;
                         }
                     }
-                    if(isPalindrome){
-                        cnt++;
+                    if(cnt == n/2){
+                        result++;
                     }
                 }
             }
-
-            System.out.println("#" + t + " " + cnt);
-
+            System.out.println("#" + t + " " + result);
         }
     }
 }
