04e147c9496e4b5c5b684da45f0369f76a5a2590
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-05-02 18:13:59 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 133 ms, Memory: 37,284 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -1,53 +1,57 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution {
+public class Solution{
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        
-        for(int t=1; t<=10; t++){
+        StringTokenizer st;
 
-           	int a = Integer.parseInt(bf.readLine());
-            int[][] arr = new int[100][100];
-            for(int i=0; i<100; i++){
-                StringTokenizer st = new StringTokenizer(bf.readLine());
-                for(int j=0; j<100; j++){
+        for(int t=1; t<=10; t++){
+            int [][] arr = new int[100][100];
+            int n = Integer.parseInt(bf.readLine());
+            for(int i=0; i<100; i++) {
+                st = new StringTokenizer(bf.readLine());
+                for (int j = 0; j < 100; j++) {
                     arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
+                int max = 0;
 
-
-            int rowMax = 0;
-            int colMax = 0;
-            int diag1Sum = 0;
-            int diag2Sum = 0;
-
-            //행 ,열 합 최대값구하기
-            for(int i=0; i<100; i++){
-                int rowSum = 0;
-                int colSum = 0;
+                //행의 합
+                for(int i=0; i<100; i++){
+                    int sum = 0;
+                    for(int j=0; j<100; j++){
+                        sum += arr[i][j];
+                    }
+                    max = Math.max(sum,max);
+                }
+                //열의 합
 
                 for(int j=0; j<100; j++){
-                    rowSum += arr[i][j]; //행 합
-                    colSum += arr[j][i]; // 열 합
+                    int sum = 0;
+                    for(int i=0; i<100; i++){
+                        sum += arr[i][j];
+                    }
+                    max = Math.max(sum,max);
                 }
-                rowMax = Math.max(rowSum,rowMax); //  행 최대값
-                colMax = Math.max(colSum,colMax); // 열 최대값
-				
-                //대각선 합 계산
-                diag1Sum += arr[i][i]; //왼쪽 상단 -> 오른쪽 하단 대각선 합
-                diag2Sum += arr[i][100-1-i]; // 오른쪽 상단 -> 왼쪽 하단 대각선 합
 
-            }
+                //대각선의 합
+                int diag_sum = 0;
+                for(int i=0; i<100; i++){
+                    for(int j=0; j<100; j++){
+                        if(i==j) {
+                            diag_sum += arr[i][j];
+                        }
+                    }
+                }
 
-            int diagonalMax = Math.max(diag1Sum,diag2Sum); // 대각선 최대값
-
-            int answer = Math.max(Math.max(rowMax,colMax),diagonalMax);
-            System.out.println("#" + t + " " + answer);
-
+                max = Math.max(diag_sum,max);
+            System.out.println("#" + t + " " + max);
 
         }
     }
