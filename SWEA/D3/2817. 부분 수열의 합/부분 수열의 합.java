b02b04fa5bc3d1d27b3115211af8d1a71194716e
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-05-02 17:56:18 +0900

    [D3] Title: 부분 수열의 합, Time: 189 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -1,44 +1,48 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    public static int n;
-    public static int k;
-    public static int[] arr;
-    public static int answer ;
+    static int n,k;
+    static int[] arr;
+    static int count;
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(bf.readLine());
-        for(int t=1; t<=T; t++){
-            StringTokenizer st = new StringTokenizer(bf.readLine());
+        StringTokenizer st;
+
+        for (int t = 1; t <= T; t++) {
+            st = new StringTokenizer(bf.readLine());
              n = Integer.parseInt(st.nextToken());
              k = Integer.parseInt(st.nextToken());
             arr = new int[n];
-             st = new StringTokenizer(bf.readLine());
-            for(int i=0; i<n; i++){
-                   arr[i] = Integer.parseInt(st.nextToken());
+            count = 0;
+            st = new StringTokenizer(bf.readLine());
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
             }
-            answer = 0;
-            recur(0,0);
-            System.out.println("#" + t + " " + answer);
-        }
 
+            dfs(0,0,0);
+
+            System.out.println("#" + t + " " + count);
+        }
     }
-
-    public static void recur(int cur, int sum){
-        if (cur > n){
-            return;
-        }
-        if(cur == n){
-            if(sum == k){
-                answer++;
+    public static void dfs(int idx, int sum, int depth){
+        // 1. 종료 조건(기저 조건)
+        if(idx == n){ // 배열 끝까지 탐색 완료
+            if(sum == k && depth > 0){ // 조건 만족(공집합 제외)
+                count++;
             }
-            return;
+            return; //종료
         }
-        recur(cur + 1, sum + arr[cur]);
-        recur(cur + 1, sum);
-    }
 
+        // 현재 원소 포함하는 경우
+        dfs(idx+1,sum+arr[idx],depth+1);
+        // 현재 원소 포함하지 않는 경우
+        dfs(idx+1,sum,depth);
+    }
 }
