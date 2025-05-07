adca58a5187bebfb1f8dde2f764627e3dae74769
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-05-08 00:26:24 +0900

    [D2] Title: 지그재그 숫자, Time: 75 ms, Memory: 24,960 KB -BaekjoonHub

--- SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.java
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,12 +10,14 @@ public class Solution {
         int T = Integer.parseInt(bf.readLine());
         for(int t=1; t<=T; t++){
             int n = Integer.parseInt(bf.readLine());
+
             int sum = 0;
             for(int i=1; i<=n; i++){
-                if(i%2 == 0){
-                    sum -= i;
-                }else{
-                    sum += i;
+                if(i%2 == 0) {
+                    sum = sum - i;
+                }
+                else{
+                    sum = sum + i;
                 }
             }
             System.out.println("#" + t + " " + sum);
