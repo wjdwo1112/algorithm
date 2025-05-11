226bd723fd51a6bb410c2eb41b76c695eb93871c
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-05-11 15:26:51 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 71 ms, Memory: 22,656 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -3,33 +3,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        
         int T = Integer.parseInt(bf.readLine());
-
         for(int t=1; t<=T; t++){
-            String a = bf.readLine();
-            int prev = 0;
-            int cnt = 0;
-            for(int i=0; i<a.length(); i++){
-                int curr = a.charAt(i) - '0';
-                if(curr != prev){
-                    cnt++;
-                    prev = curr;
-                }
+            String line = bf.readLine();
+            int [] arr = new int[line.length()];
+            for(int i=0; i<line.length(); i++){
+                arr[i] = line.charAt(i) - '0';
             }
 
+            int prev = 0;
+            int cnt = 0;
+            for(int i=0; i<arr.length; i++){
+                if(arr[i] != prev){
+                    cnt++;
+                    prev = arr[i];
+                }
+            }
             System.out.println("#" + t + " " + cnt);
 
-
-
-
-
         }
-
     }
 }
