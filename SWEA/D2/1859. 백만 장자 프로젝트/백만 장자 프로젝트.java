51390fb8f4f49ec3f20202b20ce6d3e141d49387
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-05-12 19:29:54 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 445 ms, Memory: 133,888 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
@@ -1,4 +1,5 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,31 +8,26 @@ import java.util.StringTokenizer;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         int T = Integer.parseInt(bf.readLine());
-        for (int t = 1; t <= T; t++) {
+        for(int t=1; t<=T; t++){
             int n = Integer.parseInt(bf.readLine());
             int[] arr = new int[n];
-
-            StringTokenizer st = new StringTokenizer(bf.readLine());
+            st = new StringTokenizer(bf.readLine());
             for(int i=0; i<n; i++){
                 arr[i] = Integer.parseInt(st.nextToken());
             }
-            long max = arr[n-1];
-            long sum = 0;
 
+            long max = 0;
+            long money = 0;
             for(int i=n-1; i>=0; i--){
-                if(arr[i] < max){
-                    sum += max - arr[i];
-
-                }else{
+                if(arr[i] > max){
                     max = arr[i];
-
+                }else{
+                    money = money + (max - arr[i]);
                 }
             }
-            System.out.println("#" + t + " " + sum);
-
-
+            System.out.println("#" + t + " " + money);
         }
     }
-
 }
