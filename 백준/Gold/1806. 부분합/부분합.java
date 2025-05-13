0e9ec6949b4724751885b197bbea021a855560cd
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-05-13 17:31:20 +0900

    [Gold IV] Title: 부분합, Time: 248 ms, Memory: 24352 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,50 +1,51 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int s = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n+1];
+        int[] arr = new int[n];
+
         st = new StringTokenizer(bf.readLine());
         for(int i=0; i<n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-
         int start = 0;
         int end = 0;
-        int a = 0;
-        int min = Integer.MAX_VALUE;
+        
         int sum = 0;
-        while(start <= end && end<= n){
+        int min = n+1;
 
+        while(true){
             if(sum >= s){
-                a = Math.abs(start - end) ;
-                if(min > a){
-                    min = a;
-                }
-
-                
-                sum -= arr[start];
+               
+                min = Math.min(end - start,min);
+                sum = sum - arr[start];
                 start++;
-            }else if(sum < s){
-                sum += arr[end];
+            }else if(end == n){
+                break;
+            }else{
+
+                sum = sum + arr[end];
                 end++;
             }
 
         }
-        if(min == Integer.MAX_VALUE){
+
+        if(min == n+1){
             System.out.println(0);
-        }else {
-            System.out.println(min);
+        }else{
+        System.out.println(min);
         }
     }
 }
-
