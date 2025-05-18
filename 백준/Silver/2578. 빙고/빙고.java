d536cbfc272134ff5dc8807a5d0c73d8e022e19f
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2025-05-18 17:10:29 +0900

    [Silver IV] Title: 빙고, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -1,116 +1,107 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int count ;
-    static int[][] bingo;
-    static int turn = 1;
+   static int[][] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st ;
 
-        bingo = new int[5][5];
-
-
+        arr = new int[5][5];
         for(int i=0; i<5; i++){
             st = new StringTokenizer(bf.readLine());
             for(int j=0; j<5; j++){
-                bingo[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-         count = 0;
 
+        int find = 0;
         for(int i=0; i<5; i++){
             st = new StringTokenizer(bf.readLine());
             for(int j=0; j<5; j++){
-                    int n = Integer.parseInt(st.nextToken());
-
-                    for(int k=0; k<5; k++){
-                        for(int l=0; l<5; l++){
-                            if(bingo[k][l] == n){
-                                bingo[k][l] = 0;
-                            }
+                int a = Integer.parseInt(st.nextToken());
+                find++;
+                for(int x=0; x<5; x++){
+                    for(int y=0; y<5; y++){
+                        if(arr[x][y] == a){
+                            arr[x][y] = 0;
                         }
                     }
-                    col();
-                    row();
-                    diag1();
-                    diag2();
+                }
 
-                   if(count>=3){
-                       System.out.println(turn);
-                       return;
-                   }
-                   count = 0; // 초기화
-
-                    //턴 증가
-                   turn ++;
-                   
+                int z = checkA(arr);
+                int x = checkB(arr);
+                int c = checkC(arr);
+                int v = checkD(arr);
+                if(z+x+c+v >= 3){
+                    System.out.println(find);
+                    return;
+                }
             }
         }
 
-    }
 
-    //행 체크
-    public static void col(){
+    }
+    public static int checkA(int[][] arr){
+        int count = 0;
         for(int i=0; i<5; i++){
             int cnt = 0;
             for(int j=0; j<5; j++){
-                if(bingo[j][i] == 0){
+                if(arr[i][j] == 0){
                     cnt++;
                 }
-                if(cnt == 5){
-                    count++;
-                }
-            }
-        }
-    }
 
-    //행 체크
-    public static void row(){
-        for(int i=0; i<5; i++){
-            int cnt = 0;
-            for(int j=0; j<5; j++){
-                if(bingo[i][j] == 0){
-                    cnt++;
-                }
-                if(cnt == 5){
-                    count++;
-                }
-            }
-        }
-    }
-
-    // 좌측 상단 -> 우측 하단 (대각선)
-    public static void diag1(){
-        int cnt = 0;
-        for(int i=0; i<5; i++){
-
-            if(bingo[i][i] == 0){ //(0,0), (1,1), (2,2), (3,3), (4,4)
-                cnt++;
             }
             if(cnt == 5){
                 count++;
             }
         }
+        return count;
     }
-
-    //우측상단 -> 좌측하단 (대각선)
-    public static void diag2(){
-        int cnt = 0;
+    public static int checkB(int[][] arr){
+        int count = 0;
         for(int i=0; i<5; i++){
-            if(bingo[i][4-i] == 0){ // (0,4), (1,3), (2,2), (3,1), (4,0)
-                cnt++;
+            int cnt = 0;
+            for(int j=0; j<5; j++){
+                if(arr[j][i] == 0){
+                    cnt++;
+                }
+
             }
             if(cnt == 5){
                 count++;
             }
         }
+        return count;
     }
+
+    public static int checkC(int[][] arr){
+        int count = 0;
+        int cnt = 0;
+       for(int i=0; i<5; i++){
+           if(arr[i][i] == 0)cnt++;
+       }
+       if(cnt == 5)count++;
+
+       return count;
+    }
+
+    public static int checkD(int[][] arr){
+        int count = 0;
+        int cnt = 0;
+        for(int i=0; i<5; i++){
+            if(arr[i][4-i] == 0)cnt++;
+        }
+        if(cnt == 5)count++;
+
+        return count;
+    }
+
+
+
 
 }
